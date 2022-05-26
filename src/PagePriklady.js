1440080e6695157281f25a54d97dd0063b555d53
Expert <jakubmarcek955@gmail.com>
2022-05-26 20:02:54 +0200

    hotova zakladna logika prikladov

--- src/PagePriklady.js
+++ src/PagePriklady.js
@@ -6,109 +6,134 @@
  *
  */
 
-import {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react';
 
 /* xml-file, axios, xmlparser */
-import XMLData from './priklady/priklady.xml'
-import axios from 'axios'
-import XMLParser from 'react-xml-parser'
+import XMLData from './priklady/priklady.xml';
+import axios from 'axios';
+import XMLParser from 'react-xml-parser';
 
 function PagePriklady() {
-  const [problemsList, setProblemsList] = useState([])
-  const [imageList, setImageList] = useState([])
-  const [input, setInput] = useState('')
-  const [taskNumber, setTaskNumber] = useState(NaN)
-  const [answMessage, setAnswMessage] = useState({correct: false, shouldDisplay: false})
+  const [problemsList, setProblemsList] = useState([]);
+  const [imageList, setImageList] = useState([]);
+  const [input, setInput] = useState('');
+  const [taskNumber, setTaskNumber] = useState(NaN);
+  const [answMessage, setAnswMessage] = useState({correct: false, shouldDisplay: false});
 
   useEffect(() => {
     /* nacitanie prikladov */
-    var rawXmlData = ''
+    var rawXmlData = '';
     axios
       .get(XMLData, {
         'Content-Type': 'application/xml; charset=utf-8',
       })
       .then((response) => {
-        rawXmlData = response.data
-        parseXmlData(rawXmlData)
-      })
+        rawXmlData = response.data;
+        parseXmlData(rawXmlData);
+      });
 
-    setImageList(importAllImages(require.context('./priklady/zadania', false, /.png/)))
+    setImageList(importAllImages(require.context('./priklady/zadania', false, /.png/)));
 
     /* prvotne vygenerovanie prikladu */
-    genNewTask()
-  }, [])
+    genNewTask();
+  }, []);
 
   const parseXmlData = (rawXmlData) => {
-    const xml = new XMLParser().parseFromString(rawXmlData)
-    setProblemsList(xml.children)
-  }
+    const xml = new XMLParser().parseFromString(rawXmlData);
+    setProblemsList(xml.children);
+  };
 
   const importAllImages = (r) => {
-    let images = {}
-    r.keys().map((item) => (images[item.replace('./', '')] = r(item)))
-    return images
-  }
+    let images = {};
+    r.keys().map((item) => (images[item.replace('./', '')] = r(item)));
+    return images;
+  };
 
   const genNewTask = () => {
-    setTaskNumber(Math.floor(Math.random() * 300))
-  }
+    setTaskNumber(Math.floor(Math.random() * 300));
+  };
 
-  const checkAnswer = async () => {
-    const delay = (ms) => new Promise((res) => setTimeout(res, ms))
-    console.log(problemsList[taskNumber].children[2].value)
+  const checkAnswer = async (answer) => {
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+    console.log(problemsList[taskNumber].children[2].value);
 
-    if (problemsList[taskNumber].children[2].value === input) {
-      setAnswMessage({correct: true, shouldDisplay: true})
-      await delay(1500)
-      genNewTask()
+    if (problemsList[taskNumber].children[2].value === answer) {
+      setAnswMessage({correct: true, shouldDisplay: true});
+      await delay(1500);
+      genNewTask();
     } else {
-      setAnswMessage({correct: false, shouldDisplay: true})
+      setAnswMessage({correct: false, shouldDisplay: true});
     }
-  }
+  };
+
+  const handleListClick = (e) => {
+    checkAnswer(e.currentTarget.id);
+  };
 
   const renderTask = () => {
-    if (isNaN(taskNumber)) return
+    if (isNaN(taskNumber)) return;
 
     return (
       <div>
         <h1>{problemsList[taskNumber].attributes.id}</h1>
         <img src={imageList[taskNumber + 1 + '.png']} alt="Zadanie úlohy" />
-        <label>
-          Odpoved
-          <input
-            type="text"
-            name="name"
-            onChange={(e) => {
-              setInput(e.target.value)
-            }}
-          />
-        </label>
+        {problemsList[taskNumber].children[1].value === '0' ? (
+          <div>
+            <label>
+              Odpoved
+              <input
+                type="text"
+                name="name"
+                onChange={(e) => {
+                  setInput(e.target.value);
+                }}
+              />
+            </label>
+            <button
+              type="submit"
+              value="Submit"
+              onClick={(e) => {
+                e.preventDefault();
+                checkAnswer(input);
+              }}
+            >
+              Potvrdit
+            </button>
+          </div>
+        ) : (
+          <div>
+            <ul>
+              <li id="A" onClick={handleListClick}>
+                A
+              </li>
+              <li id="B" onClick={handleListClick}>
+                B
+              </li>
+              <li id="C" onClick={handleListClick}>
+                C
+              </li>
+              <li id="D" onClick={handleListClick}>
+                D
+              </li>
+            </ul>
+          </div>
+        )}
         <button
           type="submit"
           value="Submit"
           onClick={(e) => {
-            e.preventDefault()
-            checkAnswer()
-          }}
-        >
-          Potvrdit
-        </button>
-        <button
-          type="submit"
-          value="Submit"
-          onClick={(e) => {
-            e.preventDefault()
-            genNewTask()
+            e.preventDefault();
+            genNewTask();
           }}
         >
           Dalsia
         </button>
         {answMessage.shouldDisplay && <h2>{answMessage.correct ? 'spravna odpoved' : 'nespravna odpoved'}</h2>}
       </div>
-    )
-  }
+    );
+  };
 
-  return <header>{renderTask()}</header>
+  return <header>{renderTask()}</header>;
 }
 
-export default PagePriklady
+export default PagePriklady;
